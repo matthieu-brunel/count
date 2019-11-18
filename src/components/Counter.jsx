ff9a05d4c304735063e0bb1233670af1ed6a4471
matth brunel <matth.brunel@gmail.com>
2019-11-18 10:13:23 +0100

    counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  const [count, setCount] = useState(0);
+
+  function event(event){
+    setCount(Number(event.target.value)) 
+ }
+  
+  
+  return (
+    <div>
+      <p>Le compteur est  à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button><br />
+      <input onChange={event} />
+    </div>
+  );
+}
+
+export default Counter;
